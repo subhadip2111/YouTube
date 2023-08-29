dfca0f425e44759c473e9c98e254693c2fbdc949
subhadipshee <subhadipshee20010521@gmail.com>
2023-08-29 21:33:43 +0530

    done

--- src/component/WatchPage.js
+++ src/component/WatchPage.js
@@ -13,33 +13,10 @@ const WatchPage = () => {
 const dispatch= useDispatch()
     useEffect(() => {
     dispatch(closeMenu())
-},[])
+},)
 
   return (
-//     <div className='flex-col'>
-//         <div className="flex flex-col w-full">
-//       <div className="px-5 flex w-full">
-//         <div className="">
-//           <iframe
-//             width="1200"
-//             height="600"
-//             src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-//             title="YouTube video player"
-         
-//             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-//             allowFullScreen
-//           ></iframe>
-//         </div>
-//         <div className="w-full">
-//        <LiveChat/>
-//         </div>
-//       </div>
-     
-//     </div>
 
-      
-//       <CommentsContainner/>
-//  </div>
 
     
     
