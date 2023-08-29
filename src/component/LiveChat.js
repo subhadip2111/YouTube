285492043e553fa882e4329e626fddaccfe5e3e7
subhadipshee <subhadipshee20010521@gmail.com>
2023-08-29 21:20:23 +0530

    updated

--- src/component/LiveChat.js
+++ src/component/LiveChat.js
@@ -1,79 +1,3 @@
-// import React, { useEffect, useState } from 'react'
-// import ChatMessage from './ChatMessage'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { addMessage } from '../utils/chatSlice'
-// import { generateRandomName, makeRandomMessage } from '../utils/helper'
-
-
-
-
-
-// export const LiveChat = () => {
-// const [liveMessage, setLiveMessage] = useState("");
-// const chatMessages=useSelector(store=>store.chat.messages)
-
-//     const dispatch=useDispatch()
-//          useEffect(() => {
-     
-//            const i=  setInterval(() => {
-//               //Api poling
-//                //console.log("api poling")
-               
-//                dispatch(addMessage({name:generateRandomName(),  message: makeRandomMessage(40) + " 🚀",}))
-               
-//            },2000)
-             
-             
-//              return ()=> clearInterval(i)
-//  },[])
-//   return (
- 
-      
-//        <>
-//       <div className="w-full h-[600px] ml-2 p-2 border border-red-950 bg-red-300 rounded-lg overflow-y-scroll flex flex-col-reverse">
-   
-      
-             
-//         <div>  {
-// //             // Disclaimer: Don't use indexes as keys
-//              chatMessages.map((c, i) => (
-//                <ChatMessage key={i} name={c.name} message={c.message} />
-//             ))
-//               }
-              
-// </div>
-            
-
-
-//       </div>
-
-//      <form
-//          className="w-full p-2 ml-2 border border-black"
-//          onSubmit={(e) => {
-//            e.preventDefault();
-
-//            dispatch(
-//              addMessage({
-//                name: "Subhadip",
-//                message: liveMessage,
-//              })
-//            );
-//            setLiveMessage("");
-//          }}
-//        >
-//          <input
-//            className="px-2 w-96"
-//            type="text"
-//            value={liveMessage}
-//            onChange={(e) => {
-//              setLiveMessage(e.target.value);
-//            }}
-//          />
-//          <button className="px-2 mx-2 bg-green-400">Send</button>
-//        </form>
-//     </>
-//   )
-// }
 
 
 import React, { useEffect, useState } from 'react';
@@ -98,7 +22,7 @@ export const LiveChat = () => {
     }, 2000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="flex flex-col h-[600px] bg-gradient-to-r from-blue-400 to-purple-500 rounded-lg p-4 overflow-hidden shadow-lg">
